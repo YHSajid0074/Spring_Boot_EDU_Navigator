3ddbb48485eb2d920bd12d1feeb23008a8ba1b57
Sajid <yeamimhossainsajid@gmail.com>
2024-09-19 16:10:11 +0600

    added role

---
+++ src/main/java/org/Edu/navigator/dto/response/CustomUserResponseDTO.java
@@ -0,0 +1,24 @@
+package org.Edu.navigator.dto.response;
+
+import java.util.Set;
+
+/**
+
+ */
+
+public interface CustomUserResponseDTO {
+    Long getId();
+
+
+    String getUsername();
+
+    String getEmail();
+
+    Set< RoleInfo > getRoles();
+
+    interface RoleInfo {
+        Long getId();
+
+        String getRoleType();
+    }
+}
